926e8acd739bb1768af9fc401eb3dae0914d6b77
Connie Liu <connieliu73@gmail.com>
2016-07-23 05:09:15 +0000

    Bees and Bears

--- B_Bears/game.js
+++ B_Bears/game.js
@@ -11,8 +11,8 @@ function preload() {
     backgroundImage = loadImage("honeycomb.jpg");
 }
 function setup() {
-    createCanvas(500, 250);
-    player = createSprite(25, height - 25, 0, 0);
+    createCanvas(1000, 500);
+    player = createSprite(40, height - 40, 0, 0);
     player.addImage(playerImage);
     enemy = createSprite(width - 10, height/2, 0, 0);
     enemy.addImage(enemyImage);
@@ -31,16 +31,16 @@ function draw() {
         if (keyDown(LEFT_ARROW) && player.position.x > 25) {
             player.position.x = player.position.x - 3;
         }
-        if (keyDown(38)) {
+        if (keyDown(38) && player.position.y > 50) {
             player.position.y = player.position.y - 8;
         }
-        if (keyDown(40)) {
+        if (keyDown(40) && player.position.y < height-45) {
             player.position.y = player.position.y + 8;
         }
         enemy.position.x = enemy.position.x - 8;
         if (enemy.position.x < 0) {
             enemy.position.x = width - 5;
-            enemy.position.y = random(5, width - 10);
+            enemy.position.y = random(height - 30);
         }
 
          if (enemy.overlap(player)) {
